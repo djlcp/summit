6ca57d4b1a879182828a040905fc64c6258c38b1
Jamie Le Brun <j.lebrun9@gmail.com>
2018-11-02 13:50:45 +0000

    methods added to models for start_dates and deadlines.

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,13 @@
 class Section < ApplicationRecord
 	belongs_to :project
 	has_many :tasks, dependent: :destroy
+
+
+	def start_date
+		project.start_date + commencement_day - 1
+	end
+
+	def deadline
+		start_date + duration
+	end
 end
