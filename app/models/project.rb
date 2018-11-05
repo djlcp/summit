745231658817eb59326793d78702eceacccad274
Jamie Le Brun <j.lebrun9@gmail.com>
2018-11-05 14:00:40 +0000

    completion date method added to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ApplicationRecord
 	has_many :sections, dependent: :destroy 
+
+	def completion_date
+		final_section = sections.max_by { |section| section.deadline }
+		final_section.deadline
+	end
 end
