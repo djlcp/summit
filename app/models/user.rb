d15bdd45e0d14beabf12e265cb85fa9a4bb3a643
slwwhitak <scottwhitaker123@gmail.com>
2018-11-05 11:47:16 +0000

    Revert "add invitable to model user file after dbmigrate conflict carfuffle #dont ask74cc70f567448b07854933268d23b9ea416979a7!"
    This reverts commit a0ee9eb3d83cbc68c46406114cbe8ef544494d5f.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 		
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :invitable
 end
