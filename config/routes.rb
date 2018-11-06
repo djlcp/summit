a6cfba120aca8c303e0105f67551c8795e51806b
slwwhitak <scottwhitaker123@gmail.com>
2018-11-06 17:59:02 +0000

    create controllers for user & admin model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :admins
-  root to: 'pages#home'
+  root to: 'sessions#new'
   devise_for :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
