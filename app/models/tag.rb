d872ef6d6c14132b7fd0254422ee9556a2c84a73
Jamie Le Brun <j.lebrun9@gmail.com>
2018-12-11 18:49:17 +0000

    user image added to  pin on timeline

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,4 @@
 class Tag < ApplicationRecord
-	has_many :tags_tagables, dependent: :destroy
-	has_many :tagables, through: :tags_tagables
- 	has_many :questions, through: :tags_tagables, source: :tagable, source_type: "Question"
-
+ 	has_many :tags_tagables, as: :tagable, dependent: :destroy
+	has_many :questions, through: :tags_tagables, source: :tagable, source_type: "Question"
 end
